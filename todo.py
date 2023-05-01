b2ad3e380f36b9da1e1919c1999e91263951b978
paulpls <62158353+paulpls@users.noreply.github.com>
2023-04-30 22:07:58 -0400

    upgrades for todo scraper

--- todo.py
+++ todo.py
@@ -25,6 +25,7 @@ commitMsg = "\"Updated TODO (commit performed automatically by \\`todo.py\\`)\""
 if __name__ == "__main__":
 
     out = [outhead]
+    success = False
 
     # Get list of matches
     grep = subprocess.run(cmd, shell=True, capture_output=True)
@@ -45,32 +46,44 @@ if __name__ == "__main__":
         # Add to output using the provided format
         out.append(outfmt.format(f,f,l,c))
 
-    # Remove old output file
+    # Remove old file if there are differences
     if path.exists(outpath):
-        rm = subprocess.run(f"rm {outpath}", shell=True, capture_output=True)
-
-    # Add to new output file
-    with open(outpath, "w") as f:
-        f.writelines(out)
-
-    # Auto-commit enabled
-    if autoCommit:
-        # Grep the output of git status for any added changes
-        cmd = "git status | grep -q 'Changes to be committed:'"
-        # If the quiet grep returns 0 we're no good
-        clean = not subprocess.run(cmd, shell=True).returncode
-        # If no changes have been added, auto-commit the output file
-        if clean:
-            add = f"git add {outpath}"
-            commit = f"git commit -m {commitMsg}"
-            subprocess.run(add, shell=True)
-            subprocess.run(commit, shell=True)
-        # Skip auto-commit if there are previous changes to commit
+        # Check for differences
+        with open(outpath, "r") as f:
+            lines = f.readlines()
+            try:
+                same = [out[ln] == lines[ln] for ln in range(len(out))]
+            except IndexError as e:
+                same = [false]
+                pass
+        diff = not all(same)
+        # Remove the file
+        if diff:
+            rm = subprocess.run(f"rm {outpath}", shell=True, capture_output=True)
+            success = not rm.returncode
         else:
-            print(f"Git working tree is not clean!\nSkipping auto-commit for {outpath}")
-    # Auto-commit disabled
-    else:
-        print("".join(out))
+            print("Files are the same; no changes to write.")
+
+    # Add to new output file if successful thus far
+    if success:
+        with open(outpath, "w") as f:
+            f.writelines(out)
+
+        # Auto-commit if enabled
+        if autoCommit:
+            # Grep the output of git status for any added changes
+            cmd = "git status | grep -q 'Changes to be committed:'"
+            # If the quiet grep returns 0 we're no good
+            clean = not subprocess.run(cmd, shell=True).returncode
+            # If no changes have been added, auto-commit the output file
+            if clean:
+                add = f"git add {outpath}"
+                commit = f"git commit -m {commitMsg}"
+                subprocess.run(add, shell=True)
+                subprocess.run(commit, shell=True)
+            # Skip auto-commit if there are previous changes to commit
+            else:
+                print(f"Git working tree is not clean!\nSkipping auto-commit for {outpath}")
     
 
 
